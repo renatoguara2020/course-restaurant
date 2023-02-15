205ca7527e3c9324f5182587bfa77155ef0ae5f5
Renato Alves Soares <renatoguara2020@gmail.com>
2023-02-14 21:00:18 -0300

    ok

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Admin panel of PHP</h1>
 
-echo "<h1>Hello Admin PHP</h1>";
+    <!-- menu sections start -->
+    <div class="menu">
+      Menu goes here
+    </div>
+    <!-- menu sections end -->
+
+    
+</body>
+</html>
